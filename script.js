5915f7826dde6de07b3b3e123a427c455c640bc2
Antonia <familleravel@hotmail.fr>
2023-08-18 19:19:57 +0200

    Ajout foncction bouton new game JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /*
 - click sur roll dice pour lancer le dé
 - click sur hold permet d'envoyer les points du round=curent au global et 
@@ -8,41 +9,45 @@ passe  la main à l'autre joueur
 - si le dé = 1, score round perdu et fin du tour
 - gain à 100 points
 */
+//Déclarer dans un tableau les noms des joueurs
+const playerNames = ['PLAYER 1', 'PLAYER 2'];
 
 //Déclaration des variables et remise à 0
 let gamePlaying, roundScore, activePlayer, scorePlayer;
+init();
 gamePlaying = true;
 roundScore = 0;
-activePlayer = 0;// indicateur du joueur actif: 0 premier joueur et 1 deuxième joueur
+activePlayer = 0; //indicateur du joueur actif: 0 premier joueur et 1 deuxième joueur
 scorePlayer = [0,0];
 
 
 //Masquer l'image du dé
-document.querySelector('.img-dice').display = "none";
+document.querySelector('.img-dice').style.display = "none";
 
 //Initialiser les scores à 0 au début du jeu 
 function resetScores(){
-  document.getElementById('score-0').textContent =0;
-  document.getElementById('current-0').textContent =0;
-  document.getElementById('score-1').textContent =0;
-  document.getElementById('current-1').textContent =0;
+  document.getElementById('score-0').textContent = 0;
+  document.getElementById('current-0').textContent = 0;
+  document.getElementById('score-1').textContent = 0;
+  document.getElementById('current-1').textContent = 0;
 }
 
 /*************BOUTON ROLL DICE  ****************/
+
 //fonction Math random et affichage du Dé
 document.querySelector('.btn-roll-dice').addEventListener('click', function(){
   if(gamePlaying){
-    let dice = Math.floor(Math.random()*6+ 1);
-    let diceRandom = document.querySelector('img-dice');
+    let dice = Math.floor(Math.random() *6+ 1);
+    let diceRandom = document.querySelector('.img-dice');
     diceRandom.style.display = 'block';
-    diceRandom.src = 'IMG/dice-'+ dice +'.png';
+    diceRandom.src = 'IMG/dice-' + dice + '.png';
     
     /*Si le dé est différent de 1, le joueur continue de jouer. 
     On ajoute le résultat du dé au score global du joueur.*/
     /*Si le dé est égal à 1, le joueur actif perd son score temporaire (on le remet à zéro) et le tour passe au joueur suivant en utilisant la fonction nextPlayer().*/
     
     if (dice !== 1){
-      roundScore += dice;
+      roundScore +=dice;
       document.getElementById('current-'+activePlayer).textContent = roundScore;
     }
     else{
@@ -52,16 +57,19 @@ document.querySelector('.btn-roll-dice').addEventListener('click', function(){
 });
 
 /*************BOUTON HOLD  ****************/
+
 //fonction  qui permet d'ajoutée la valeur du score temporaire au score total du joueur et passe son tour à l'autre joueur.
 document.querySelector('.btn-hold').addEventListener('click', function(){
   if(gamePlaying){
     scorePlayer[activePlayer] += roundScore;
     //Mise à jour du score du joueur actif.
     document.getElementById('score-'+activePlayer).textContent = scorePlayer[activePlayer];
+    
     if(scorePlayer[activePlayer] >= 10){
+      
       //Variables gagnant ou perdant
       const winnerIndex = activePlayer;
-      //Si activePlayer n'est pas === 0, joueur 2 gagne et joueur 1 perd
+      //Si activePlayer ===0 le joueur 1 a perdu, activerPlayer à 1 le joueur 2 a perdu 
       const loserIndex = activePlayer === 0 ? 1 : 0;
       
       //Affiche le gagnant
@@ -84,12 +92,10 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     }else{
       changePlayer();
     }
-
   }
 });
 
-
-/*La fonction changePlayer() permet de passer au joueur suivant. Si activePlayer est égal à 0, on le met à 1, sinon on le remet à 0.*/
+//La fonction changePlayer() permet de passer au joueur suivant. Si activePlayer est égal à 0, on le met à 1, sinon on le remet à 0
 function changePlayer(){
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
   roundScore = 0;
@@ -102,3 +108,48 @@ function changePlayer(){
   document.querySelector('.playerContainer-1-panel').classList.toggle('active');
   //.toggle permet d ajouter la classe active s'il n y en a pas
 }
+
+  /*************BOUTON NEW GAME ****************/
+document.querySelector('.btn-new-game').addEventListener('click', init);
+let newScore = [0,0];// Ajouter une nouvelle variable pour stocker les scores totaux
+
+//RESET GLOBAL (function init)
+function init(){
+  //réinitialiser à 0
+  gamePlaying = true;
+  roundScore = 0;
+  activePlayer = 0;
+  scorePlayer = [0,0];
+
+  //on recache le dé
+  document.querySelector('.img-dice').display = 'none';
+
+  //Réintialiser les noms des joueurs
+  function updatePlayers() {
+    document.getElementById('player-0').textContent = "PLAYER 1";
+    document.getElementById('player-1').textContent = "PLAYER 2";
+  };
+
+  //Remettre en noir la police des joueurs
+  function resetColors() {
+    document.getElementById('player-0').style.color = '#000'; 
+    document.getElementById('player-1').style.color = '#000'; 
+  };
+
+  // Supprimer les classes 
+  function resetClassPlayers() {
+    document.querySelector(".playerContainer-0-panel").classList.remove("winner");
+    document.querySelector(".playerContainer-1-panel").classList.remove("winner");
+    document.querySelector(".playerContainer-0-panel").classList.remove("active");
+    document.querySelector(".playerContainer-1-panel").classList.remove("active");
+  };
+  
+  //Appel des fonctions
+  resetClassPlayers();
+  resetScores();
+  updatePlayers();
+  resetColors();
+
+  //Le joueur 1 est actif au début du jeu
+  document.querySelector('.playerContainer-0-panel').classList.add("active");
+};
