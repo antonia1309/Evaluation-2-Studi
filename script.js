f7f8651f696618678a8796716806fa6794b2a5e9
Antonia <familleravel@hotmail.fr>
2023-08-18 17:44:20 +0200

    Ajour fonction bouton Hold

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ passe  la main à l'autre joueur
 */
 
 //Déclaration des variables et remise à 0
-let gamePlaying, roundScore, activePlayer;
+let gamePlaying, roundScore, activePlayer, scorePlayer;
 gamePlaying = true;
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 0;// indicateur du joueur actif: 0 premier joueur et 1 deuxième joueur
+scorePlayer = [0,0];
 
 
 //Masquer l'image du dé
@@ -29,8 +30,7 @@ function resetScores(){
 
 /*************BOUTON ROLL DICE  ****************/
 //fonction Math random et affichage du Dé
-document.querySelector('.btn-roll-dice').addEventListener(
-  click', function(){
+document.querySelector('.btn-roll-dice').addEventListener('click', function(){
   if(gamePlaying){
     let dice = Math.floor(Math.random()*6+ 1);
     let diceRandom = document.querySelector('img-dice');
@@ -39,10 +39,11 @@ document.querySelector('.btn-roll-dice').addEventListener(
     
     /*Si le dé est différent de 1, le joueur continue de jouer. 
     On ajoute le résultat du dé au score global du joueur.*/
+    /*Si le dé est égal à 1, le joueur actif perd son score temporaire (on le remet à zéro) et le tour passe au joueur suivant en utilisant la fonction nextPlayer().*/
     
     if (dice !== 1){
-      document.getElementById('current-'+activePlayer).textContent = roundScore;
       roundScore += dice;
+      document.getElementById('current-'+activePlayer).textContent = roundScore;
     }
     else{
       changePlayer();
@@ -50,8 +51,54 @@ document.querySelector('.btn-roll-dice').addEventListener(
   }
 });
 
-/*Si le dé est égal à 1, le joueur actif perd son score temporaire (on le remet à zéro) et le tour passe au joueur suivant en utilisant la fonction nextPlayer(). 
-La fonction changePlayer() permet de passer au joueur suivant. Si activePlayer est égal à 0, on le met à 1, sinon on le remet à 0.*/
-function changePlayer(){
+/*************BOUTON HOLD  ****************/
+//fonction  qui permet d'ajoutée la valeur du score temporaire au score total du joueur et passe son tour à l'autre joueur.
+document.querySelector('.btn-hold').addEventListener('click', function(){
+  if(gamePlaying){
+    scorePlayer[activePlayer] += roundScore;
+    //Mise à jour du score du joueur actif.
+    document.getElementById('score-'+activePlayer).textContent = scorePlayer[activePlayer];
+    if(scorePlayer[activePlayer] >= 10){
+      //Variables gagnant ou perdant
+      const winnerIndex = activePlayer;
+      //Si activePlayer n'est pas === 0, joueur 2 gagne et joueur 1 perd
+      const loserIndex = activePlayer === 0 ? 1 : 0;
+      
+      //Affiche le gagnant
+      const winnerName = document.getElementById('player-' + winnerIndex);
+      winnerName.textContent = playerNames[winnerIndex] + " WINNER";
+      winnerName.style.color = '#3CB371';
 
+      //Affiche le nom du joueur perdant
+      const loserName = document.getElementById('player-' + loserIndex);
+      loserName.textContent = playerNames[loserIndex] + " FAILED";
+      loserName.style.color = '#EB4D4C';
+
+      document.querySelector('.img-dice').style.display ='none';
+      //Ajout d'une classe winner du joueur gagnant
+      document.querySelector('.playerContainer-'+activePlayer+'-panel').classList.add('winner');
+      //Retrait de la classe active du joueur gagnant
+      document.querySelector('.playerContainer-'+activePlayer+'-panel').classList.remove('active');
+      //Le jeu est terminé quand le joueur atteint le score final
+      gamePlaying = false;
+    }else{
+      changePlayer();
+    }
+
+  }
+});
+
+
+/*La fonction changePlayer() permet de passer au joueur suivant. Si activePlayer est égal à 0, on le met à 1, sinon on le remet à 0.*/
+function changePlayer(){
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = 0;
+  document.getElementById('current-1').textContent = 0;
+
+  //Désactive ou active l'apparence des joueurs qui jouent
+  document.querySelector('.playerContainer-0-panel').classList.toggle('active');
+  document.querySelector('.playerContainer-1-panel').classList.toggle('active');
+  //.toggle permet d ajouter la classe active s'il n y en a pas
 }
